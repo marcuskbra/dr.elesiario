b02d2706b5a4db12ed9e75ea6d5582672f5f5992
Marcus Carvalho <marcuskbra@hotmail.com>
2015-03-15 12:47:09 -0300

    Ajustes no layout da pagina de contato e configuraçao da barra de rolagem para estar sempre visivel

--- projeto/js/script.js
+++ projeto/js/script.js
@@ -65,10 +65,10 @@ $(document).ready(function ($) {
 	$("html").niceScroll({
 		scrollspeed: 100,
 		mousescrollstep: 38,
-		cursorwidth: 5,
+		cursorwidth: 10,
 		cursorborder: 0,
 		cursorcolor: '#333',
-		autohidemode: true,
+		autohidemode: false,
 		zindex: 999999999,
 		horizrailenabled: false,
 		cursorborderradius: 0,
